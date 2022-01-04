fb32d7a47ad4f54092ba3476b44eb5b5b82bd3e4
stepa-b <borozdin.stepan@gmail.com>
2022-01-04 12:03:10 +0300

    Added debugging 3 new files

--- Debug1/binary search  recursively.py
+++ Debug1/binary search  recursively.py
@@ -1,3 +1,6 @@
+import time
+start = time.perf_counter()
+
 def find_item(list, item):
   #Returns True if the item is in the list, False if not.
   if len(list) == 0:
@@ -9,7 +12,7 @@ def find_item(list, item):
     return True
 
   #Is the item in the first half of the list?
-  if item < list[middle]:
+  if item in list[:middle]:
     #Call the function with the first half of the list
     return find_item(list[:middle], item)
   else:
@@ -25,3 +28,5 @@ print(find_item(list_of_names, "Alex")) # True
 print(find_item(list_of_names, "Andrew")) # False
 print(find_item(list_of_names, "Drew")) # True
 print(find_item(list_of_names, "Jared")) # False
+end = time.perf_counter()
+print(end-start)
