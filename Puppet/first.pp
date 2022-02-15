630fa8bbc5af2996c6f38c3c440c3ee3af756d57
Stepa-mac-kotik <borozdin@playkot.com>
2022-02-15 20:25:57 +0300

    End of the course

---
+++ Puppet/first.pp
@@ -0,0 +1,57 @@
+class machine_info {
+   file { '/tmp/machine_info.txt':
+       content => template('machine_info/info.erb'),
+   }
+   if $facts[kernel] == "windows" {
+       $info_path = "C:\Windows\Temp\Machine_Info.txt"
+   } else {
+       $info_path = "/tmp/machine_info.txt"
+   }
+}
+
+
+class machine_info {
+  if $facts[kernel] == "windows" {
+       $info_path = "C:\Windows\Temp\Machine_Info.txt"
+   } else {
+       $info_path = "/tmp/machine_info.txt"
+   }
+ file { 'machine_info':
+       path => $info_path,
+       content => template('machine_info/info.erb'),
+   }
+}
+
+class machine_info {
+  if $facts[kernel] == "windows" {
+       $info_path = "C:\Windows\Temp\Machine_Info.txt"
+   } else {
+       $info_path = "/tmp/machine_info.txt"
+   }
+ file { 'machine_info':
+       path => $info_path,
+       content => template('machine_info/info.erb'),
+   }
+}
+
+class reboot {
+  if $facts[kernel] == "windows" {
+    $cmd = "shutdown /r"
+  } elsif $facts[kernel] == "Darwin" {
+    $cmd = "shutdown -r now"
+  } else {
+    $cmd = "reboot"
+  }
+  if $facts[uptime_days] > 30 {
+    exec { 'reboot':
+      command => $cmd,
+     }
+   }
+}
+
+
+node default {
+   class { 'packages': }
+   class { 'machine_info': }
+   class { 'reboot': }
+}
